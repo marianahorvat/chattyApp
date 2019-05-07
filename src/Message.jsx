fd4753e7864fedb0542b9dbde088fb2396cbd088
Mariana <mariana.horvat.qa@gmail.com>
2019-05-07 21:39:50 +0000

    Data Flow in Chatty

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,8 +4,8 @@ class Message extends Component {
   render() {
     return (
       <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
+        <span className="message-username">{this.props.currentUsername}</span>
+        <span className="message-content">{this.props.currentMessage}</span>
       </div>
       // <div class="message system">
       //   Anonymous1 changed their name to nomnom.
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,54 +4,22 @@ import Message from "./Message.jsx";
 class MessageList extends Component {
   constructor (props) {
     super(props);
-    this.state = {}
-    this.state.messages = [
-      {
-        type: "incomingMessage",
-        content: "I won't be impressed with technology until I can download food.",
-        username: "Anonymous1"
-      },
-      {
-        type: "incomingNotification",
-        content: "Anonymous1 changed their name to nomnom",
-      },
-      {
-        type: "incomingMessage",
-        content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-        username: "Anonymous2"
-      },
-      {
-        type: "incomingMessage",
-        content: "...",
-        username: "nomnom"
-      },
-      {
-        type: "incomingMessage",
-        content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-        username: "Anonymous2"
-      },
-      {
-        type: "incomingMessage",
-        content: "This isn't funny. You're not funny",
-        username: "nomnom"
-      },
-      {
-        type: "incomingNotification",
-        content: "Anonymous2 changed their name to NotFunny",
-      },
-    ];
   }
    
   render() {
     return (
       <main className="messages">
-        {/* <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
-        </div> */}
-        <div className="message system">
+        { this.props.messages.map((data, index) =>
+          <Message
+                  key={ index }
+                  currentUsername={ data.username }
+                  currentMessage={ data.content }
+          />
+        )}
+
+        {/* <div className="message system">
           Anonymous1 changed their name to nomnom.
-        </div>
+        </div> */}
       </main>
     );
   }
